e0a4bedbe896ba72eeb655abf14dfa392de164af
guedonotfound <g.guedes.a@hotmail.com>
2025-06-24 08:29:34 -0300

    chore: update package-lock and add 'use client' in authentication components

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SignUpForm from "./components/sign-up-form";
 import LoginForm from "./components/login-form";
