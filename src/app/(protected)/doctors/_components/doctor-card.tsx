4c910d6cf558f80317bdb811698b36d5d0d705f8
guedonotfound <g.guedes.a@hotmail.com>
2025-07-03 11:27:26 -0300

    feat: add doctor delete button and functionality

--- src/app/(protected)/doctors/_components/doctor-card.tsx
+++ src/app/(protected)/doctors/_components/doctor-card.tsx
@@ -17,11 +17,30 @@ import {
 } from "@/components/ui/dialog";
 import { Separator } from "@/components/ui/separator";
 import { doctorsTable } from "@/db/schema";
-import { CalendarIcon, ClockIcon, DollarSignIcon } from "lucide-react";
+import {
+  CalendarIcon,
+  ClockIcon,
+  DollarSignIcon,
+  TrashIcon,
+} from "lucide-react";
 import UpsertDoctorForm from "./upsert-doctor-form";
 import { getAvailability } from "../_helpers/availability";
 import { formatCurrencyInCents } from "@/helpers/currency";
 import { useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogTitle,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogTrigger,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from "@/components/ui/alert-dialog";
+import { useAction } from "next-safe-action/hooks";
+import { toast } from "sonner";
+import { deleteDoctor } from "@/actions/delete-doctor";
 
 interface DoctorCardProps {
   doctor: typeof doctorsTable.$inferSelect;
@@ -42,6 +61,21 @@ const DoctorCard = ({ doctor }: DoctorCardProps) => {
       : (doctorName[0] ?? "");
 
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const deleteDoctorAction = useAction(deleteDoctor, {
+    onSuccess: () => {
+      toast.success("Médico excluído com sucesso");
+    },
+    onError: () => {
+      toast.error("Erro ao excluir médico");
+    },
+  });
+
+  const handleDeleteDoctorClick = () => {
+    deleteDoctorAction.execute({
+      id: doctor.id,
+    });
+  };
 
   return (
     <Card>
@@ -74,22 +108,48 @@ const DoctorCard = ({ doctor }: DoctorCardProps) => {
       </CardContent>
       <Separator />
       <CardFooter>
-        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-          <DialogTrigger asChild>
-            <Button className="w-full">Ver detalhes</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogTitle>Ver detalhes</DialogTitle>
-            <UpsertDoctorForm
-              doctor={{
-                ...doctor,
-                availableFromTime: availability.from.format("HH:mm:ss"),
-                availableToTime: availability.to.format("HH:mm:ss"),
-              }}
-              onSuccess={() => setIsDialogOpen(false)}
-            />
-          </DialogContent>
-        </Dialog>
+        <div className="flex w-full justify-center gap-1">
+          <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <DialogTrigger asChild>
+              <Button className="flex-1">Ver detalhes</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogTitle>Ver detalhes</DialogTitle>
+              <UpsertDoctorForm
+                doctor={{
+                  ...doctor,
+                  availableFromTime: availability.from.format("HH:mm:ss"),
+                  availableToTime: availability.to.format("HH:mm:ss"),
+                }}
+                onSuccess={() => setIsDialogOpen(false)}
+              />
+            </DialogContent>
+          </Dialog>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive">
+                <TrashIcon />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>
+                  Tem certeza que deseja excluir este médico?
+                </AlertDialogTitle>
+                <AlertDialogDescription>
+                  Essa ação não pode ser desfeita. Após a exclusão, todos os
+                  agendamentos do médico serão cancelados.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                <AlertDialogAction onClick={handleDeleteDoctorClick}>
+                  Excluir
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
       </CardFooter>
     </Card>
   );
