53fb2968cdfc77628c492e78590413480a236f0a
guedonotfound <g.guedes.a@hotmail.com>
2025-07-08 15:25:47 -0300

    feat: implement patient deletion

--- src/actions/delete-patient/index.ts
+++ src/actions/delete-patient/index.ts
@@ -6,6 +6,8 @@ import { actionClient } from "@/lib/next-safe-action";
 import { z } from "zod";
 import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 const schema = z.object({
   id: z.string().uuid(),
@@ -14,6 +16,17 @@ const schema = z.object({
 export const deletePatient = actionClient
   .schema(schema)
   .action(async ({ parsedInput }) => {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+    if (!session?.user) throw new Error("Unauthorized");
+
+    const patient = await db.query.patientsTable.findFirst({
+      where: eq(patientsTable.id, parsedInput.id),
+    });
+    if (!patient || patient.clinicId != session.user.clinic?.id)
+      throw new Error("Paciente não encontrado");
+
     await db.delete(patientsTable).where(eq(patientsTable.id, parsedInput.id));
     revalidatePath("/patients");
   });
--- src/app/(protected)/patients/_components/table-actions.tsx
+++ src/app/(protected)/patients/_components/table-actions.tsx
@@ -19,6 +19,20 @@ import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
 import { patientsTable } from "@/db/schema";
 import { useState } from "react";
 import UpsertPatientForm from "./upsert-patient-form";
+import { useAction } from "next-safe-action/hooks";
+import { deletePatient } from "@/actions/delete-patient";
+import { toast } from "sonner";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 interface PatientTableActionsProps {
   patient: typeof patientsTable.$inferSelect;
@@ -26,41 +40,79 @@ interface PatientTableActionsProps {
 
 const PatientTableActions = ({ patient }: PatientTableActionsProps) => {
   const [upsertDialogIsOpen, setUpsertDialogIsOpen] = useState(false);
+
+  const deletePatientAction = useAction(deletePatient, {
+    onSuccess: () => {
+      toast.success("Paciente deletado com sucesso.");
+    },
+    onError: () => {
+      toast.error("Erro ao deletar paciente.");
+    },
+  });
+
+  const handleDeletePatientClick = () => {
+    if (!patient) return;
+    deletePatientAction.execute({ id: patient.id });
+  };
+
   return (
-    <Dialog open={upsertDialogIsOpen} onOpenChange={setUpsertDialogIsOpen}>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="icon" className="h-8 w-8">
-            <MoreVerticalIcon />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel className="font-semibold">
-            {patient.name}
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => setUpsertDialogIsOpen(true)}>
-            <EditIcon />
-            Editar
-            <DialogContent>
-              <DialogTitle>Editar Paciente</DialogTitle>
-              <DialogDescription>
-                Edite as informações do paciente.
-              </DialogDescription>
-            </DialogContent>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <TrashIcon />
-            Excluir
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <UpsertPatientForm
-        patient={patient}
-        isOpen={upsertDialogIsOpen}
-        onSuccess={() => setUpsertDialogIsOpen(false)}
-      />
-    </Dialog>
+    <>
+      <Dialog open={upsertDialogIsOpen} onOpenChange={setUpsertDialogIsOpen}>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <MoreVerticalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel className="font-semibold">
+              {patient.name}
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => setUpsertDialogIsOpen(true)}>
+              <EditIcon />
+              Editar
+              <DialogContent>
+                <DialogTitle>Editar Paciente</DialogTitle>
+                <DialogDescription>
+                  Edite as informações do paciente.
+                </DialogDescription>
+              </DialogContent>
+            </DropdownMenuItem>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                  <TrashIcon />
+                  Excluir
+                </DropdownMenuItem>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Tem certeza que deseja excluir este paciente?
+                  </AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Essa ação não pode ser desfeita. Após a exclusão, todos os
+                    agendamentos do paciente serão cancelados.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                  <AlertDialogAction onClick={handleDeletePatientClick}>
+                    Excluir
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <UpsertPatientForm
+          patient={patient}
+          isOpen={upsertDialogIsOpen}
+          onSuccess={() => setUpsertDialogIsOpen(false)}
+        />
+      </Dialog>
+    </>
   );
 };
 
